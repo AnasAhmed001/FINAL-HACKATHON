6301c3b6afa41ddcaeb14bfeb975fc72a386519b
AnasAhmed001 <hafizanasahmed8@gmail.com>
2025-01-26 08:17:33 +0500

    corrected

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://mammoth-lorelei-anasahmed-90354937.koyeb.app/",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
